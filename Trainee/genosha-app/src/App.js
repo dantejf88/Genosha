f0a4232dd61185d6a6ce1516d0a8f3c50eb1bc87
dantejf88 <dantejf88@gmail.com>
2018-09-25 13:32:30 -0300

    subiendo a Github

---
+++ Trainee/genosha-app/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <div className="Biggest">
+            <div className="Middlest">
+              <div className="Smallest"></div>
+            </div>
+          </div>
+          <h1 className="Title">GENOSHA</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
